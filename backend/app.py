86b00f1be7b814e0b25bf615ee3e47cdc776a41e
Michael van der Loo <michaeljvanderloo@gmail.com>
2022-03-29 10:07:21 -0400

    fixed imports

--- backend/app.py
+++ backend/app.py
@@ -6,7 +6,7 @@ from sectagtog.sec_tagtog import SECTagTog
 
 app = Flask("fraxure_backend")
 CORS(app, origins=["http://localhost:3000"])
-s = SECTagTog()
+sectagtog = ""
 
 
 @app.route("/time")
@@ -16,9 +16,9 @@ def get_current_time():
 
 @app.route("/tagtog/init/<ttusername>/<ttpassword>")
 def init_tagtog(ttusername, ttpassword, projname):
-    s = SECTagTog(ttusername, ttpassword, " ", projname)
+    sectagtog = SECTagTog(ttusername, ttpassword, " ", projname)
 
 
 @app.route("/tagtog/post/<form_type>/<ticker>")
 def post_tagtog(form_type, ticker):
-    s.mostRecent(form_type, ticker)
+    sectagtog.mostRecent(form_type, ticker)
