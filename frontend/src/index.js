3525f5cb21ec36872ef275852d1eddc8d10bd7fe
Michael Vanderloo <michaelvanderloo@icloud.com>
2022-05-02 17:32:22 -0400

    stuff

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,8 +6,6 @@ import App from "./App";
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
 
-// https://stackoverflow.com/questions/41956465/how-to-create-multiple-page-app-using-react
-
 const linker = document.querySelector(".linkbtn");
 linker.addEventListener("click", function () {
   window.open(
@@ -16,30 +14,6 @@ linker.addEventListener("click", function () {
   );
   console.log("button clicked");
 });
-
-/*
-const switcher = document.querySelector('.mbtn');
-
-switcher.addEventListener('click', function() {   
-    var element = document.getElementById('wrapper');
-    var class_ = element.className;
-
-    if (class_ === "dark-theme") {
-        console.log('Dark theme --> Light Theme'); 
-        element.classList.toggle('dark-theme'); 
-        element.classList.toggle('light-theme');   
-        this.textContent = "Light";
-    }  
-    else if(class_ === "light-theme") {
-        console.log('Light theme --> Dark Theme'); 
-        element.classList.toggle('light-theme');  
-        element.classList.toggle('dark-theme');  
-        this.textContent = "Dark"; 
-    } 
-    console.log('current class name: ' + element.className);
-
-});
-*/
 
 const searcher = document.querySelector(".searchbtn");
 
@@ -69,15 +43,6 @@ searcher.addEventListener("click", function () {
   // var url = "localhost:5000/tagtog/init/" + username.value + "/" + password.value + "/" + project.value + "/" + type.value + "/" + cik.value + "/" + pre.value + "/" + post.value;
 
   fetch(url) 
-
-  
-  /*
-  window.open(
-    url,
-    "_blank"
-  ); 
-  window.close()
-  */
   
   var url = "http://www.tagtog.net/" + username.value + "/" + project.value + "/"
   fetch(url) 
