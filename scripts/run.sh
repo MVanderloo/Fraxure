4aa2d931234d31e389ac88f5c091329fb7f8cab9
Michael van der Loo <michaeljvanderloo@gmail.com>
2022-05-03 12:23:57 -0400

    removed unnecessary files

--- scripts/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# assumes setup was run recently
-
-python backend/app/app.py
--- scripts/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-source venv/bin/activate
-pip freeze > requirements.txt
-pip install -r requirements.txt
-
-# npm install
